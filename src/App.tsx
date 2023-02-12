94d2632def288abb2c6dfac1ea88edb1c191989e
Lavínia Beghini <lavinia.beghini@creditas.com>
2023-02-12 20:49:53 +0100

    Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
 import 'react-native-gesture-handler';
 
-import {
-  DarkTheme as NavigationDarkTheme,
-  DefaultTheme as NavigationDefaultTheme,
-} from '@react-navigation/native';
-import merge from 'deepmerge';
 import { registerRootComponent } from 'expo';
 import React from 'react';
-import {
-  MD3DarkTheme,
-  MD3LightTheme,
-  adaptNavigationTheme,
-  Provider as PaperProvider,
-} from 'react-native-paper';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 import { PreferencesContext } from './configuration/PreferencesContext';
 import RootNavigation from './navigation/RootNavigation';
-
-const { LightTheme, DarkTheme } = adaptNavigationTheme({
-  reactNavigationLight: NavigationDefaultTheme,
-  reactNavigationDark: NavigationDarkTheme,
-});
-
-const CombinedDefaultTheme = merge(MD3LightTheme, LightTheme);
-const CombinedDarkTheme = merge(MD3DarkTheme, DarkTheme);
+import { CombinedDarkTheme, CombinedDefaultTheme } from './utils/theme';
 
 function App() {
   const [isThemeDark, setIsThemeDark] = React.useState(false);
